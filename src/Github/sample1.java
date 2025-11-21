0914ba4f3a2a860ee2f3e20ddf641a001a05a67d
bhadajay <jaybhadage@gmail.com>
2025-11-21 18:42:34 +0530

    First Github class

---
+++ src/Github/sample1.java
@@ -0,0 +1,22 @@
+package Github;
+
+public class sample1 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("first Github project");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}

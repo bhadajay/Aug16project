b225b627eb7202f3f4fcc174608994f26eb5d9e1
bhadajay <jaybhadage@gmail.com>
2025-11-24 17:03:15 +0530

    Sample2 committed

---
+++ src/Collection/sample2.java
@@ -0,0 +1,37 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class sample2
+{
+
+public static void main(String[] args) {
+		
+		LinkedList LL= new LinkedList();
+		LL.add("Jayesh");
+		LL.add(200);
+	    LL.add(2, 3);
+	    LL.add(null);
+	    LL.add(25.1f);
+	    LL.add(null);
+		LL.add('F');
+		System.out.println(LL);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
